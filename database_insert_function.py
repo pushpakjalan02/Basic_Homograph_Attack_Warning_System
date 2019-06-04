453324a918cd3d41f8ee9752c36dea37d31b9b71
Pushpak Jalan <pushpakjalan07@outlook.com>
2019-06-04 11:23:20 +0530

    database_insert_function.py was made user friendly by supporting more than one URL insertion at the same time

--- database_insert_function.py
+++ database_insert_function.py
@@ -9,11 +9,21 @@ def main():
     )
     print(my_db_connection)
     pointer = my_db_connection.cursor()
-    url = input("Enter URL\n");
-    query = "INSERT INTO urls VALUES ('"+ url +"');"
-    pointer.execute(query)
-    my_db_connection.commit()
-    print(pointer.rowcount, "Record Inserted...")
+    print('Enter 0 when done')
+    list_to_append = []
+    j = 0
+    while(True):
+        list_to_append.append(input())
+        if list_to_append[j] == '0':
+            break
+        j += 1
+    i = 0
+    while i < j:
+        query = "INSERT INTO urls VALUES ('"+ list_to_append[i] +"');"
+        pointer.execute(query)
+        my_db_connection.commit()
+        i += 1
+    print(j, "Record Inserted...")
 
 if __name__ == '__main__':
     main()
